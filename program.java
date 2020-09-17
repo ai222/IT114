da412374f96ca897974e745d82ca6dfdcecec3ac
ai222 <60273815+ai222@users.noreply.github.com>
2020-09-17 00:42:11 -1200

    addprogram

---
+++ program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Homework{
+public static void main(String[] args){
+String stringName = "This is a Text";
+    System.out.println(stringName);
+     int age = 20;
+     System.out.println("age ="+ age);
+     Double var=9.99;
+     System.out.println(var);
+    
+}
+}
